06ee9367c6dfda0562af38472c728fddc9e1d86d
Merlin Osayimwen <merlinosayimwen@gmail.com>
2019-04-30 00:55:32 +0200

    Add mroe tests for TwoPhaseSet

--- src/test/java/io/github/merlinosayimwen/crdt/TwoPhaseSetTest.java
+++ src/test/java/io/github/merlinosayimwen/crdt/TwoPhaseSetTest.java
@@ -4,8 +4,14 @@
 
 package io.github.merlinosayimwen.crdt;
 
-import io.github.merlinosayimwen.crdt.set.TwoPhaseSet;
+import java.util.ArrayList;
+import java.util.Collection;
+
 import junit.framework.TestCase;
+
+import com.google.common.collect.Lists;
+
+import io.github.merlinosayimwen.crdt.set.TwoPhaseSet;
 
 /**
  * Tests the TwoPhaseSet methods.
@@ -13,6 +19,68 @@ import junit.framework.TestCase;
  * @see TwoPhaseSet
  */
 public final class TwoPhaseSetTest extends TestCase {
+
+  /**
+   * Creates a TwoPhaseSet from a collection of distinct strings and asserts
+   * that the set contains all of the elements.
+   */
+  public void testCreation() {
+    Collection<String> initialElements = Lists.newArrayList("a", "b", "c");
+    TwoPhaseSet<String> set = TwoPhaseSet.of(initialElements);
+    assertContains(set, initialElements);
+  }
+
+  /**
+   * Creates a TwoPhaseSet from a collection and asserts that modifications
+   * to the collection do not affect the set.
+   */
+  public void testDefensive() {
+    Collection<String> initial = new ArrayList<>();
+    TwoPhaseSet<String> set = TwoPhaseSet.of(initial);
+    // The TwoPhaseSet has to make a defensive copy of the initial-elements
+    // arguments. Therefor adding an element to the initial-elements should
+    // not add it to the sets elements.
+    initial.add("d");
+    assertFalse(set.contains("d"));
+  }
+
+  /**
+   * Tries to create a TwoPhaseSet from a list with a null element and
+   * asserts that a NullPointerException is thrown.
+   */
+  public void testCreation_nullElement() {
+    Collection<String> initialElements = Lists.newArrayList("a", "b", null);
+    try {
+      TwoPhaseSet.of(initialElements);
+      fail("Expected TwoPhaseSet#of to throw NPE on null element in argument list");
+    } catch (NullPointerException expected) {
+      // This exception is expected
+    }
+  }
+
+  private void assertContains(TwoPhaseSet<String> set, Collection<String> elements) {
+    for (String element : elements) {
+      assertTrue(set.contains(element));
+    }
+  }
+
+  /**
+   * Creates a set with N different initial elements and asserts that the
+   * number returned by {@code size()} is N.
+   */
+  public void testSize_differentElements() {
+    TwoPhaseSet<String> set = TwoPhaseSet.of("a", "b", "c");
+    assertEquals(set.size(), 3);
+  }
+
+  /**
+   * Creates a set with initial elements that contain duplications and
+   * asserts that the size equals the amount of distinct elements.
+   */
+  public void testSize_equalElements() {
+    TwoPhaseSet<String> set = TwoPhaseSet.of("a", "b", "b");
+    assertEquals(set.size(), 2);
+  }
 
   /**
    * Merges a set with a tombstone for element A, with another
@@ -40,6 +108,14 @@ public final class TwoPhaseSetTest extends TestCase {
 
     set.add("a");
     assertFalse(set.contains("a"));
+  }
+
+  /**
+   * Creates a new empty set and asserts that it has no tombstones.
+   */
+  public void testTombstoneCreationAtInitialization() {
+    TwoPhaseSet<String> set = TwoPhaseSet.empty();
+    assertEquals(set.tombstones().size(), 0);
   }
 
   /**
