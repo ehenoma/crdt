efcf4bbd1564c3c914f01a911d70a9edf4199bba
Merlin Osayimwen <merlinosayimwen@gmail.com>
2019-04-29 02:04:57 +0200

    Improve test coverage of TwoPhaseSet

--- src/test/java/io/github/merlinosayimwen/crtd/TwoPhaseSetTest.java
+++ src/test/java/io/github/merlinosayimwen/crtd/TwoPhaseSetTest.java
@@ -4,7 +4,6 @@
 
 package io.github.merlinosayimwen.crtd;
 
-import io.github.merlinosayimwen.crtd.set.ReplicatedSet;
 import io.github.merlinosayimwen.crtd.set.TwoPhaseSet;
 import junit.framework.TestCase;
 
@@ -16,16 +15,58 @@ import junit.framework.TestCase;
 public final class TwoPhaseSetTest extends TestCase {
 
   /**
-   * Calls {@link ReplicatedSet#remove(Object)} ona set and asserts that the element has not been
-   * removed from the instance and is not present in the returned updated set.
+   * Merges a set with a tombstone for element A, with another
+   * set that contains element A and ensures that A is not
+   * in the merge result and a tombstone for A exists.
    */
-  public void testTombstones() {
-    TwoPhaseSet<String> set = TwoPhaseSet.of("a", "b", "c");
-    assertTrue(set.contains("a"));
-
+  public void testTombstoneWinsMerge() {
+    TwoPhaseSet<String> set = TwoPhaseSet.empty();
     set.remove("a");
+
+    TwoPhaseSet<String> merged = set.merge(TwoPhaseSet.of("a"));
+    assertFalse(merged.contains("a"));
+    assertTombstoneCreated(merged, "a");
+  }
+
+  /**
+   * Creates a tombstone for element A, then tries to add A
+   * to the set and ensures that it fails by checking whether
+   * A is within the set after the call to add.
+   */
+  public void testRemoveWins() {
+    TwoPhaseSet<String> set = TwoPhaseSet.empty();
+    set.remove("a");
+    assertTombstoneCreated(set, "a");
+
+    set.add("a");
     assertFalse(set.contains("a"));
-    assertTrue(set.tombstones().contains("a"));
+  }
+
+  /**
+   * Removes an element from a set that does not contain the element and
+   * ensures that a tombstone is also created if the element is not in
+   * the set at the time that {@code remove} is called. This is crucial
+   * for coordination free use of this type.
+   */
+  public void testTombstoneCreation_elementAbsent() {
+    testTombstoneCreation(TwoPhaseSet.empty(), "a");
+  }
+
+  /**
+   * Removes an element from a set that contains the element and
+   * ensures that a tombstone is created.
+   */
+  public void testTombstoneCreation_elementPresent() {
+    testTombstoneCreation(TwoPhaseSet.of("a"), "a");
+  }
+
+  private void testTombstoneCreation(TwoPhaseSet<String> set, String element) {
+    set.remove(element);
+    assertTombstoneCreated(set, element);
+  }
+
+  private void assertTombstoneCreated(TwoPhaseSet<String> set, String element) {
+    assertTrue(set.tombstones().contains(element));
   }
 
   /**
